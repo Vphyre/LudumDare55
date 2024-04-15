b3c9b4cfedfef2c11cf420f58c993ced9d3264fc
vphyre <vinipaulnic@hotmail.com>
2024-04-15 19:58:54 -0300

    Added All Stages

--- Assets/Scripts/Menu/DifficultyManager.cs
+++ Assets/Scripts/Menu/DifficultyManager.cs
@@ -2,11 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DifficultyManager : MonoBehaviour
 {
+    public UnityEvent OnEasy;
+    public UnityEvent OnMedium;
+    public UnityEvent OnHard;
     public void SetDifficulty(int difficulty)
     {
-        PlayerPrefs.SetInt("difficulty", difficulty);
+        PlayerPrefs.SetInt("difficulty", difficulty); // Easy 1, Medium 2, Hard 3
+    }
+    private void Awake()
+    {
+        ReadDifficulty();
+    }
+    public void ReadDifficulty()
+    {
+        if (PlayerPrefs.GetInt("difficulty") == 1)
+        {
+            OnEasy.Invoke();
+        }
+        else if (PlayerPrefs.GetInt("difficulty") == 2)
+        {
+            OnMedium.Invoke();
+        }
+        else if (PlayerPrefs.GetInt("difficulty") == 3)
+        {
+            OnHard.Invoke();
+        }
     }
 }
